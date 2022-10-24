12c60496ff099317dd0ec721d57389e2da5013b2
VaibhavAgarwal452 <>
2022-10-24 18:39:57 +0530

    feat: Create index.html

---
+++ 1 - two sums.js
@@ -0,0 +1,16 @@
+const nums = [2, 4, 7, 8];
+const target = 10;
+let vals = {};
+function twoSum(nums, target) {
+  let vals = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (target - nums[i] in vals) {
+      return [vals[target - nums[i]], i];
+    } else {
+      vals[nums[i]] = i;
+    }
+  }
+  return [];
+}
+console.log(twoSum(nums, target));
